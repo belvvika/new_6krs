111551c0a28d157717f3612988f183230fbfddbb
belvvika <134885414+belvvika@users.noreply.github.com>
2024-11-05 01:30:10 +0300

    new

--- clients/urls.py
+++ clients/urls.py
@@ -14,20 +14,20 @@ urlpatterns = [
     path('', cache_page(60)(NewsletterListView.as_view()), name='newsletter_list'),
     path('create/', NewsletterCreateView.as_view(), name='newsletter_create'),
     path('detail/<int:pk>/', NewsletterDetailView.as_view(), name='newsletter_detail'),
-    path('update/<int:pk>/', NewsletterUpdateView.as_view(), name='newsletter_update'),
-    path('delete/<int:pk>/', NewsletterDeleteView.as_view(), name='newsletter_delete'),
+    path('update/<int:pk>/', NewsletterUpdateView.as_view(), name='newsletter_form'),
+    path('delete/<int:pk>/', NewsletterDeleteView.as_view(), name='newsletter_confirm_delete'),
 
     path('client/', ClientListView.as_view(), name='client_list'),
     path('client/create/', ClientCreateView.as_view(), name='client_create'),
     path('client/detail/<int:pk>/', ClientDetailView.as_view(), name='client_detail'),
-    path('client/update/<int:pk>/', ClientUpdateView.as_view(), name='client_update'),
-    path('client/delete/<int:pk>/', ClientDeleteView.as_view(), name='client_delete'),
+    path('client/update/<int:pk>/', ClientUpdateView.as_view(), name='client_form'),
+    path('client/delete/<int:pk>/', ClientDeleteView.as_view(), name='client_confirm_delete'),
 
     path('message/', MessageListView.as_view(), name='message_list'),
     path('message/create/', MessageCreateView.as_view(), name='message_create'),
     path('message/detail/<int:pk>/', MessageDetailView.as_view(), name='message_detail'),
-    path('message/update/<int:pk>/', MessageUpdateView.as_view(), name='message_update'),
-    path('message/delete/<int:pk>/', MessageDeleteView.as_view(), name='message_delete'),
+    path('message/update/<int:pk>/', MessageUpdateView.as_view(), name='message_form'),
+    path('message/delete/<int:pk>/', MessageDeleteView.as_view(), name='message_confirm_delete'),
 
     path('log_list/', MailingAttemptListView.as_view(), name='mailingattempt_list')
 
